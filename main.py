2736130164e4299e2845bb484fd0ae464ed6c7f2
laundmo <laurinschmidt2001@gmail.com>
2021-08-16 01:06:40 +0200

    Using PySide6, Popup window working

--- main.py
+++ main.py
@@ -1,126 +1,79 @@
-# Import Library
-from tkinter import Entry, Listbox, StringVar, Tk, END
-import tkinter.font as font
+from PySide6 import QtCore, QtWidgets, QtGui
 from pynput import keyboard
-from pynput.keyboard import Key, Controller
-import pyperclip
+from snippet_handler import SnippetsSearch
+import os
 
-from win32gui import GetForegroundWindow, SetForegroundWindow
+windows = False
+if os.name == "nt":
+    import win32gui, win32con, win32process, win32api
+    win32gui.SystemParametersInfo(win32con.SPI_SETFOREGROUNDLOCKTIMEOUT, 0, win32con.SPIF_SENDWININICHANGE | win32con.SPIF_UPDATEINIFILE)
+    windows = True
 
+class MyLineEdit(QtWidgets.QLineEdit):
+    def focusOutEvent(self, event):
+        self.parentWidget().toggle_visible()
+        
+class MyWidget(QtWidgets.QWidget):
+    def __init__(self, search):
+        super().__init__()
 
-# Create Object
-root = Tk()
+        self.button = QtWidgets.QPushButton("Hide!")
+        self.input = MyLineEdit()
+        self.layout = QtWidgets.QVBoxLayout(self)
+        self.layout.addWidget(self.button)
+        self.layout.addWidget(self.input)
 
-# Set title
-root.title("Main Window")
+        self.button.clicked.connect(self.toggle_visible)
+        self.input.textChanged.connect(self.input_text_changed)
+        
+        self.search = search
+        self.input.setWindowModality(QtCore.Qt.ApplicationModal)
+        self.setWindowState(QtCore.Qt.WindowActive)
+        self.setWindowFlags(self.windowFlags() | QtCore.Qt.WindowStaysOnTopHint)
+        self.setFocusPolicy(QtCore.Qt.StrongFocus)
+    
+    @QtCore.Slot()
+    def toggle_visible(self):
+        if self.isVisible():
+            print("Hiding popup")
+            self.hide()
+        else:
+            print("Showing popup")
+            self.show()
+            if windows:
+                fgwin = win32gui.GetForegroundWindow()
+                fg = win32process.GetWindowThreadProcessId(fgwin)[0]
+                current = win32api.GetCurrentThreadId()
+                if current != fg:
+                    win32process.AttachThreadInput(fg, current, True)
+                    win32gui.SetForegroundWindow(self.winId())
+                    win32process.AttachThreadInput(fg, win32api.GetCurrentThreadId(), False)
+            self.activateWindow()
+            self.input.raise_()
+            self.input.grabKeyboard()
+            self.input.setFocus()
+    
+    @QtCore.Slot()
+    def input_text_changed(self, text):
+        self.search.search_snippet(text)
 
-# Set Geometry
-root.geometry("400x200")
+class KeybindPressed(QtCore.QObject):
+    keybind_pressed = QtCore.Signal()
 
-font_cmu = font.Font(
-    root=root,
-    font=(
-        "CMU Serif",
-        20,
-    ),
-    name="CMU Serif",
-)
-
-snippetname = StringVar()
-
-text = Entry(root, textvariable=snippetname, font=font_cmu, justify="center")
-text.pack()
-
-
-def on_key(*args):
-    update_listbox()
-
-
-snippetname.trace("w", on_key)
-
-
-class MyListbox(Listbox):
-    def __init__(self, *args, **kwargs):
-        Listbox.__init__(self, *args, **kwargs)
-        self.current_selection = 0
-
-    def select(self, index):
-        self.select_clear(0, "end")
-        self.selection_set(index)
-        self.see(index)
-        self.activate(index)
-        self.selection_anchor(index)
-
-    def up(self, *args):
-        self.current_selection -= 1
-        self.select(self.current_selection)
-
-    def down(self, *args):
-        self.current_selection += 1
-        self.select(self.current_selection)
-
-
-lb = MyListbox(root, font=font_cmu)
-lb.pack()
-
-root.bind("<Up>", lb.up)
-root.bind("<Down>", lb.down)
-
-all_items = ["test1", "test2", "test3"]
+    def __call__(self):
+        self.keybind_pressed.emit()
 
 
-def update_listbox(*args):
-    search_term = snippetname.get()
-    lb.delete(0, END)
-    for item in all_items:
-        if search_term.lower() in item.lower():
-            lb.insert(END, item)
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+    search = SnippetsSearch()
 
+    pressed = KeybindPressed()
+    with keyboard.GlobalHotKeys({"<cmd>+<space>": pressed}):
 
-prev_window = None
+        widget = MyWidget(search)
+        pressed.keybind_pressed.connect(widget.toggle_visible)
+        widget.resize(200, 200)
+        widget.show()
 
-def show():
-    global prev_window
-    prev_window = GetForegroundWindow()
-
-    root.deiconify()
-    text.focus()
-
-
-# Hide the window
-def hide():
-    root.withdraw()
-    if prev_window:
-        SetForegroundWindow(prev_window)
-
-
-is_hidden = False
-
-
-def check_state():
-    global is_hidden
-    if is_hidden:
-        show()
-    else:
-        hide()
-    is_hidden = not is_hidden
-
-
-keyboard.GlobalHotKeys({"<cmd>+<space>": check_state}).__enter__()
-
-keyboard = Controller()
-
-def paste_and_close(*args): # TODO: why does this only trigger when the mouse is hovering on top of the window?
-    text = lb.get(lb.curselection())
-    pyperclip.copy(text)
-    hide()
-    keyboard.press(Key.ctrl)
-    keyboard.press("v")
-    keyboard.release("v")
-    keyboard.release(Key.ctrl)
-
-
-root.bind("<Enter>", paste_and_close)
-
-# Execute Tkinter
-root.mainloop()
+        app.exec()
